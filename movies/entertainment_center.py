1934c7296773d47b29a2aeb76a8928f63bb504ab
hellourgo <madden.jeremy@gmail.com>
2015-10-23 13:33:42 -0700

    Added movies and call to show movie page

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,4 +1,4 @@
-import media
+import media, fresh_tomatoes
 
 toy_story = media.Movie(
     "Toy Story",
@@ -7,8 +7,6 @@ toy_story = media.Movie(
     "https://www.youtube.com/watch?v=KYz2wyBy3kc"
 )
 
-print(toy_story.storyline)
-
 avatar = media.Movie(
     "Avatar",
     "A marine on an alien planet",
@@ -16,6 +14,27 @@ avatar = media.Movie(
     "https://www.youtube.com/watch?v=5PSNL1qE6VY"
 )
 
-print(avatar.title)
+fifth_element = media.Movie(
+    "The Fifth Element",
+    "A lady made from aliens saves the world by kissing Bruce Willis",
+    "https://upload.wikimedia.org/wikipedia/en/6/65/Fifth_element_poster_%281997%29.jpg",
+    "https://www.youtube.com/watch?v=aB-AUTGqUCU"
+)
 
-avatar.show_trailer()
+matrix = media.Movie(
+    "The Matrix",
+    "A guy learns how to beat up computer programs",
+    "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+    "https://www.youtube.com/watch?v=vKQi3bBA1y8"
+)
+
+pacific_rim = media.Movie(
+    "Pacific Rim",
+    "Giant robots fight giant monsters",
+    "https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg",
+    "https://www.youtube.com/watch?v=5guMumPFBag"
+)
+
+movies = [toy_story, avatar, fifth_element, matrix, pacific_rim]
+
+fresh_tomatoes.open_movies_page(movies)
