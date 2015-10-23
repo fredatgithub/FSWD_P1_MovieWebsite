90b4fe5df3bbd5c942cb3c5b8303e9ce5c4d288a
hellourgo <madden.jeremy@gmail.com>
2015-10-23 14:06:00 -0700

    Added lead role and IMDB Url

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -4,42 +4,54 @@ toy_story = media.Movie(
     "Toy Story",
     "A story of a boy and his toys that come to life",
     "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-    "https://www.youtube.com/watch?v=KYz2wyBy3kc"
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc",
+    "Tom Hanks",
+    "http://www.imdb.com/title/tt0114709/?ref_=nv_sr_1"
 )
 
 avatar = media.Movie(
     "Avatar",
     "A marine on an alien planet",
     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-    "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY",
+    "Sigourney Weaver",
+    "http://www.imdb.com/title/tt0499549/?ref_=nv_sr_1"
 )
 
 fifth_element = media.Movie(
     "The Fifth Element",
     "A lady made from aliens saves the world by kissing Bruce Willis",
     "https://upload.wikimedia.org/wikipedia/en/6/65/Fifth_element_poster_%281997%29.jpg",
-    "https://www.youtube.com/watch?v=aB-AUTGqUCU"
+    "https://www.youtube.com/watch?v=aB-AUTGqUCU",
+    "Chris Tucker",
+    "http://www.imdb.com/title/tt0119116/?ref_=nv_sr_1"
 )
 
 matrix = media.Movie(
     "The Matrix",
     "A guy learns how to beat up computer programs",
     "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-    "https://www.youtube.com/watch?v=vKQi3bBA1y8"
+    "https://www.youtube.com/watch?v=vKQi3bBA1y8",
+    "Keanu Reeves",
+    "http://www.imdb.com/title/tt0133093/?ref_=nv_sr_1"
 )
 
 pacific_rim = media.Movie(
     "Pacific Rim",
     "Giant robots fight giant monsters",
     "https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg",
-    "https://www.youtube.com/watch?v=5guMumPFBag"
+    "https://www.youtube.com/watch?v=5guMumPFBag",
+    "Gypsy Danger",
+    "http://www.imdb.com/title/tt1663662/?ref_=nv_sr_1"
 )
 
 boxtrolls = media.Movie(
     "The Boxtrolls",
     "A delusional child is held hostage by garbage-gnomes",
     "https://upload.wikimedia.org/wikipedia/en/d/db/The_Boxtrolls_poster.jpg",
-    "https://www.youtube.com/watch?v=Q2dFVnp5K0o"
+    "https://www.youtube.com/watch?v=Q2dFVnp5K0o",
+    "Ben Kingsley",
+    "http://www.imdb.com/title/tt0787474/"
 )
 
 movies = [toy_story, avatar, fifth_element, matrix, pacific_rim, boxtrolls]
--- movies/media.py
+++ movies/media.py
@@ -2,12 +2,22 @@ import webbrowser
 
 
 class Movie():
+    """Defines the movie data structure"""
+
     def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
+                 trailer_youtube, movie_lead, movie_imdb):
+        """Initialize the movie object
+
+        :param movie_title: Name of the movie
+        :param movie_storyline: Short description of the movie
+        :param poster_image: URL to an image of the movie's poster
+        :param trailer_youtube: URL to a YouTube video of the movie's trailer
+        :param movie_lead: Leading actor/actress
+        :param movie_imdb: URL to the movie's IMDB page
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        self.leading_role = movie_lead
+        self.imdb_url = movie_imdb
